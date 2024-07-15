67f134a282513dcb893d79755911de326b503669
Cristian Munoz <cgmuros@gmail.com>
2024-07-15 14:36:03 -0400

    Changed how to read variables

--- app/config.py
+++ app/config.py
@@ -1,8 +1,4 @@
-from dotenv import load_dotenv
 import os
 
-# Carga las variables de entorno desde el archivo .env
-load_dotenv()
 
-# Obtiene las variables de entorno
 SECRET_KEY = os.getenv("SECRET_KEY")
